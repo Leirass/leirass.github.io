9c2b9a841c783d309811cee90b8c3cf65d3db845
leirass <sarielperalta0819@gmail.com>
2023-06-29 19:06:59 -0500

    presinamediogo

---
+++ index.js
@@ -0,0 +1,3 @@
+function cambiarImagen(){
+    document.getElementById("img1").src="   https://www.debate.com.mx/__export/1491334879891/sites/debate/img/2017/04/04/macaco_crop1491334754748.jpg_423682103.jpg";
+}
