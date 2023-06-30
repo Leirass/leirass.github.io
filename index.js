875e5a7817be2578f8b890dda98bb24d5dbd16c2
leirass <sarielperalta0819@gmail.com>
2023-06-29 20:08:39 -0500

    sisa

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function cambiarImagen(){
-    document.getElementById("img1").src="   https://www.debate.com.mx/__export/1491334879891/sites/debate/img/2017/04/04/macaco_crop1491334754748.jpg_423682103.jpg";
+    document.getElementById("img1").src="   https://i.ytimg.com/vi/rGARC8KYmCY/maxresdefault.jpg";
 }
 
